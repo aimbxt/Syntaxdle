94f6f0f7f32aa74cfaa828b5c10c4ae45c297312
aimbxtl <067alexl@gmail.com>
2026-08-08 23:00:35 -0600

    feat(security): add express-rate-limiter to protect api routes

--- Wordle Clone/backend/src/app.js
+++ Wordle Clone/backend/src/app.js
@@ -2,16 +2,18 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const session = require('express-session')
+const rateLimit = require('express-rate-limit');
 const app = express();
 const port = process.env.PORT;
 const usersRouter = require('./routes/users')
 const guessRouter = require('./routes/guess')
 
-
 app.use(cors({
   credentials: true
 }))
 app.use(express.json())
+
+//express-session
 app.use(session({
   secret: process.env.SESSION_SECRET,
   saveUninitialized: false,
@@ -23,6 +25,14 @@ app.use(session({
     maxAge: 60000 * 60
   }
 }))
+
+//rate-limiter
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests, please try again later.'
+}))
+
 app.use('/api/user', usersRouter)
 app.use('/api/guess', guessRouter) 
 
